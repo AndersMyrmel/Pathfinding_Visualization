9029ec413d47f8607618ee1da2790b76647d57e5
Anders <andersmyrmel@gmail.com>
2022-05-24 23:36:09 +0200

    added grid

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
 import pygame
 import sys
 
-WIDTH = 900
+WIDTH = 800
+BLACK = (0, 0, 0)
+WHITE = (230, 230, 230)
 
-pygame.init()
-
-display = pygame.display.set_mode((WIDTH, WIDTH))
-pygame.display.set_caption('Pathfinding Visualization')
-
+def drawGrid():
+    blockSize = 40
+    for x in range(0, WIDTH, blockSize):
+        for y in range(0, WIDTH, blockSize):
+            rect = pygame.Rect(x, y, blockSize, blockSize)
+            pygame.draw.rect(display, BLACK, rect, 1)
 
 def main():
+    global display
+    pygame.init()
+    display = pygame.display.set_mode((WIDTH, WIDTH))
+    pygame.display.set_caption('Pathfinding Visualization')
+    display.fill(WHITE)
+
     while True:
-          # creating a loop to check events that are occurring
+        drawGrid()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-        # updating the display
-        pygame.display.flip()
+
+        pygame.display.update()
 
 if __name__ == '__main__':
     main()
